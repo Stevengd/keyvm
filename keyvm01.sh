9beb637ac1b0257b843b7f73178f2d770c290504
Stevengd <40215915+Stevengd@users.noreply.github.com>
2023-06-27 01:39:58 -0700

    Update keyvm01.sh

--- keyvm01.sh
+++ keyvm01.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wget -O  https://github.com/Stevengd/keyvm/blob/37fa006e58d8e3299359da086c0a9d04b7011f9f/host_setup.sh
-wget -O  https://github.com/Stevengd/keyvm/blob/37fa006e58d8e3299359da086c0a9d04b7011f9f/keyvm.tar
+wget https://github.com/Stevengd/keyvm/blob/37fa006e58d8e3299359da086c0a9d04b7011f9f/host_setup.sh
+wget https://github.com/Stevengd/keyvm/blob/37fa006e58d8e3299359da086c0a9d04b7011f9f/keyvm.tar
 
 sudo chmod +x host_setup.sh
 ./host_setup.sh
